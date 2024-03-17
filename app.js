9b757aed75166dce490bae06c3f2a926d137490f
Michael Johnson <elsif.maj@gmail.com>
2024-03-17 13:30:32 -0700

    Serve Swagger UI doc

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const fCountRouter = require('./routes/fCountRouter');
 const db = require('./database/db');
 const queries = require('./database/queries');
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = YAML.load('./openApi3.yml');
 
 app.use(express.json());
 
@@ -11,5 +14,7 @@ queries.createFCountTable();
 
 // Base path for the API 
 app.use('/f-counts', fCountRouter);
+// API documentation
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 module.exports = app;
